d3a1f46ee2867eaa112e70b68cced827ce931b76
gageb438 <gageb534@gmail.com>
2025-08-18 13:08:21 -0500

    Update Program2-23.cpp

--- Chapter 2/Program2-23/Program2-23/Program2-23.cpp
+++ Chapter 2/Program2-23/Program2-23/Program2-23.cpp
@@ -1,1 +1,20 @@
-// ran outa time, slide 81.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// program 2-23
+	// program 2-23 calculates the same price of an item that has a regular
+	// price of $59.95, with a 20 percent discount applied
+
+	float basePrice = 59.95;
+	float discountPercent = .20;
+	float discountPrice = basePrice * discountPercent;
+	float endPrice = basePrice - discountPrice;
+
+	cout << "Regular Price: $" << basePrice << endl;
+	cout << "Discount amount: $" << discountPrice << endl;
+	cout << "Sale Price: $" << endPrice << endl;
+
+	return 0;
+}
