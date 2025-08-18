be523de879ecdbb6ba828c528d3fcdc003422605
gageb438 <gageb534@gmail.com>
2025-08-18 13:49:09 -0500

    nm,

---
+++ Chapter 2/Exercises/Exercise2/Exercise2/Exercise2.cpp
@@ -0,0 +1,16 @@
+// Sales prediction accepts no arguments
+// it calculates the midwest division income
+// outputs the above
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// create variables
+	const float sales1 = 8.6, sales2 = 10.0;
+	float output1, output2;
+	float percent = .58;
+	
+	output1 = 
+}
