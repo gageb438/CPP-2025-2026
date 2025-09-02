f04e5b44d9e7e608464143c78a7e6c23e44028ec
Gage <gageb534@gmail.com>
2025-09-02 13:40:10 -0500

    fooomyd

--- Chapter3/Exercises/Exercise3-25/Exercise3-25/Exercise3-25.cpp
+++ Chapter3/Exercises/Exercise3-25/Exercise3-25/Exercise3-25.cpp
@@ -16,38 +16,30 @@ int main()
 
 	// take input from user
 	cout << "Input a name: ";
-	cin.ignore();
 	getline(cin, name);
 	
 	cout << "Input an age: ";
-	cin.ignore();
 	getline(cin, age);
 
-
 	cout << "Input the name of a city: ";
-	cin.ignore();
 	getline(cin, city);
 
 	cout << "Input the name of a college: ";
 	getline(cin, college);
-	cin.ignore();
 
 	cout << "Input the name of a profession: ";
 	getline(cin, profession);
-	cin.ignore();
 
 	cout << "Input a type of animal: ";
 	getline(cin, animal);
-	cin.ignore();
-
+	
 	cout << "Input a pets name: ";
 	getline(cin, petsname);
-	cin.ignore();
 
-	cout << "There was a person named " << name << " who decided FOR SOME REASON to live in " << city << ". THEN once they turned " << age << " " << name << " went to COLLEGE (woohooo!!) "
-		<< "and then they graduated!!! YAY!!" << " He then wanted to work as a " << profession << " but then " << name << " had a change of heart and realized that the J word is a slur!! "
-		<< " After this new revelation, " << name << " burned their degree. " << name << " then adopted a " << animal << " named " << petsname << " and lived for a long time!! but then he realized that his "
-		<< animal << " was a robot!!" << " he yelled clanker when he found out and got arrested for using fighting words because that word had been outlawed in 2025. " << name << " later died in jail.";
+	cout << "\nThere was a person named " << name << " who decided FOR SOME REASON to live in " << city << ".\nTHEN once they turned " << age << " " << name << " went to COLLEGE (woohooo!!) "
+		<< "and then they graduated!!! YAY!!\n" << "They then wanted to get a j*b as a " << profession << " but then " << name << " had a change of heart and realized that the J word is a slur!!"
+		<< "\nThey realized they had enough money to buy a labubu and bought a golden one.\nAfter this new revelation, " << name << " burned their degree. " << name << " then adopted a " << animal << " named " << petsname << " and lived for a long time!!\nBut then he realized that his "
+		<< animal << " was a robot!!" << " they yelled clanker when he found out and got arrested for using fighting words \nbecause that word had been outlawed in 2025.\n" << name << " later died in jail.";
 
 	return 0;
 }
