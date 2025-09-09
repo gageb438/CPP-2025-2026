6c3cd6ee4617cb1ed72089f10a502ff69aeccc1d
Gage <gageb534@gmail.com>
2025-09-09 13:56:26 -0500

    Update Chapter 4 Exercises.cpp

--- Chapter 4/Exercises/Chapter 4 Exercises/Chapter 4 Exercises/Chapter 4 Exercises.cpp
+++ Chapter 4/Exercises/Chapter 4 Exercises/Chapter 4 Exercises/Chapter 4 Exercises.cpp
@@ -26,45 +26,120 @@ int main()
 	cout << "------------------------------------" << endl;
 
 	// validate input
-	if (choice >= 1 && choice <= 6)
+	switch (choice)
 	{
-		// process the menu
-		if (choice == 1)
-		{
-			cout << "Exercise 4-11 - Math Tutor Revision" << endl << endl;
+	case 1:
+	{
+		cout << "Exercise 4-11 - Math Tutor Revision" << endl << endl;
 
-			// do program1 here.
-		}
-		else if (choice == 2)
-		{
-			cout << "Exercise 4-17 - Pole Vault Personal Best" << endl << endl;
+		/*
+		Exercise 3-17
+		Generates a random number from 1-500
+		adds them together
+		and displays the answer
+		*/
 
-			// ``
-		}
-		else if (choice == 3)
-		{
-			cout << "Exercise 4-23 - Gemometry Calculator" << endl << endl;
+		// give a seed and create variables
+		unsigned seed = time(0);
+		srand(seed);
 
-			// ``
-		}
-		else if (choice == 4)
-		{
-			cout << "Exercise 4-27 - WiFi Diagnostic" << endl << endl;
+		int maxNum = 500,
+			minNum = 1,
+			num1 = (rand() % (maxNum - minNum + 1)) + minNum,
+			num2 = (rand() % (maxNum - minNum + 1)) + minNum,
+			output = num1 + num2,
+			answer = 0,
+			correct_answer = num1 + num2;
 
-			// ``
-		}
-		else if (choice == 5)
+		// output the math problem
+		cout << "  " << num1 << endl;
+		cout << "+ " << num2 << endl;
+		cout << "------" << endl;
+
+		// take input for the answer
+		cout << endl << "Input your answer here :> ";
+		cin >> answer;
+
+		if (answer == correct_answer)
 		{
-			cout << "Exercise 4-28 - Restaurant Selector" << endl << endl;
+			cout << "Your answer was correct.";
 		}
 		else
 		{
-			cout << "Thank you for using the Exercise Selection System." << endl << endl;
+			cout << "Your answer was incorrect.";
+			// output the answer and problem again/
+			cout << "\n  " << num1 << endl;
+			cout << "+ " << num2 << endl;
+			cout << "------" << endl;
+			cout << "  " << output << endl;
+		}
+		return 0;
+	}
+	case 2:
+	{
+		cout << "Exercise 4-17 - Personal Best" << endl << endl;
+
+		/*
+		4-17 asks for the name of a pole vaulter, the dates, and the height in meters of the athletes 3 best vaults
+		It then reports in order of height, best first, the date on which each vault was made
+		outputs ^
+		*/
+
+		// intiialize variables
+		string name,
+			vault1date,
+			vault2date,
+			vault3date;
+
+		double vault1height,
+			vault2height,
+			vault3height;
+		// take input
+		cout << "Enter the pole vaulter's name: ";
+		cin >> name;
+
+		cout << "Enter the date of the first pole vault: ";
+		cin >> vault1date;
+
+		cout << "Enter the height of the first pole vault: ";
+		cin >> vault1height;
+
+		cout << "Enter the date of the second pole vault: ";
+		cin >> vault2date;
+
+		cout << "Enter the height of the second pole vault: ";
+		cin >> vault2height;
+
+		cout << "Enter the date of the third pole vault: ";
+		cin >> vault3date;
+
+		cout << "Enter the height of the third pole vault: ";
+		cin >> vault3height;
+
+		if (!(vault1height >= 2 && vault1height <= 5 && vault2height >= 2 && vault2height <= 5 && vault3height >= 2 && vault3height <= 5))
+		{
+			cout << "All pole vault heights must be greater than or equal to 2 and less than or equal to 5.";
+
+			return 0;
+		}
+		else
+		{
+			if (vault1height >= vault2height && vault2height >= vault3height)
+			{
+				cout << "The best vault took place on " << vault1date << " with a height of " << vault1height << endl;
+				cout << "The 2nd best vault took place on " << vault2date << " with a height of " << vault2height << endl;
+				cout << "The 3rd best vault took place on " << vault3date << " with a height of " << vault3height << endl;
+			}
+			else if (vault1height >= vault2height && vault2height <= vault3height)
+			{
+				cout << "The best vault took place on " << vault1date << " with a height of " << vault1height << endl;
+				cout << "The 2nd best vault took place on " << vault3date << " with a height of " << vault3height << endl;
+				cout << "The 3rd best vault took place on " << vault2date << " with a height of " << vault2height << endl;
+			}
+			else if (vault2height >= vault1height && vault1height >= vault3height)
+			{
+				cout << "The best vault took place on " << vault2date << " with a height of " << vault2height << endl;
+				cout << "The 2nd best vault took place on " << vault1date << " with a height of " << vault1height << endl;
+				cout << "The 3rd best vault took place on " << vault3date << " with a height of " << vault3height << endl;
 		}
 	}
-	// catch any input error
-	else
-	{
-		cout << endl << endl << "Invalid choice, try again" << endl << endl;
-	}
-}
