3172b76f8174dbab082a3b7335ebe8aabb5caee0
gageb438 <gageb534@gmail.com>
2026-02-11 20:52:33 -0600

    mrs lafever talks in cryptic code bro

--- Chapter 10/Chapter 10 Practices/Chapter 10 Practices.cpp
+++ Chapter 10/Chapter 10 Practices/Chapter 10 Practices.cpp
@@ -1,126 +1,148 @@
 #include <iostream>
+#include <string>
 #include <cctype>
 
 using namespace std;
 
+void Program_10_1();
+
+void Program_10_2_A();
+bool Program_10_2_B(char Customer_Number_Array[], int SIZE);
+
+void Program_10_3();
+
 int main()
 {
-	int choice = 2;
+	return 0;
+}
 
-	switch (choice)
+
+void Program_10_1()
+{
+	char User_Input;
+
+	cout << "Enter a character: ";
+	cin.get(User_Input);
+
+	if (isalpha(User_Input))
 	{
-	case 1:
-	{
-		string again = "y";
-		while (again == "y");
+		cout << "This is a alpha character." << endl;
+
+		if (isupper(User_Input))
 		{
-			char Users_Character;
-
-			cout << "Please enter a keyboard character: ";
-			cin.ignore();
-			cin.get(Users_Character);
-
-			if (isalpha(Users_Character))
-			{
-				cout << "That is an alpha character." << endl << endl;
-			}
-			if (isalnum(Users_Character))
-			{
-				cout << "That is an alphanumeric character." << endl << endl;
-			}
-			if (isdigit(Users_Character))
-			{
-				cout << "That character is a digit." << endl << endl;
-			}
-			if (islower(Users_Character))
-			{
-				cout << "That character is lowercase." << endl << endl;
-			}
-			if (isupper(Users_Character))
-			{
-				cout << "That character is uppercase." << endl << endl;
-			}
-			if (isprint(Users_Character))
-			{
-				cout << "That character is a printable character." << endl << endl;
-			}
-			if (ispunct(Users_Character))
-			{
-				cout << "That is a printable character." << endl << endl;
-			}
-			if (isspace)
-			{
-				cout << "That is a space." << endl << endl;
-			}
-
-			cout << "Again? >> ";
-			cin >> again;
-		}
-	}
-	case 2:
-	{
-		string Customer_Number;
-		bool Passing = true;
-		char Number[8];
-
-		cout << "Please enter your 8 digit customer number: ";
-		cin >> Customer_Number;
-
-		for (int Iterator = 0; Iterator < 7; Iterator++)
-		{
-			Number[Iterator] = Customer_Number[Iterator];
-		}
-
-		for (int Iterator = 0; Iterator < 7; Iterator++)
-		{
-			if (Iterator <= 2)
-			{
-				if (isalpha(Number[Iterator]))
-				{
-					continue;
-				}
-				else
-				{
-					Passing = false;
-				}
-			}
-			else
-			{
-				if (isdigit(Number[Iterator]))
-				{
-					continue;
-				}
-				else
-				{
-					Passing = false;
-				}
-			}
-		}
-
-		if (Passing)
-		{
-			cout << "Account number accepted." << endl;
+			cout << "This is a uppercase character." << endl;
 		}
 		else
 		{
-			cout << "Invalid account number!" << endl;
+			cout << "This is a lowercase character." << endl;
 		}
 	}
-	case 3:
+	else if (isdigit(User_Input))
 	{
-		char Users_Character;
-
-		cout << "Please enter a keyboard character to make uppercase: ";
-		cin.ignore();
-		cin.get(Users_Character);
-		
-		Users_Character = toupper(Users_Character);
-
-		cout << "That character uppercased is " << Users_Character << endl;
+		cout << "This is a digit." << endl;
 	}
-	case 4:
+	else if (ispunct(User_Input))
 	{
-
+		cout << "This is a punctuation mark." << endl;
 	}
+
+	if (isprint(User_Input))
+	{
+		cout << "This is a printable character." << endl;
 	}
 }
+
+
+void Program_10_2_A()
+{
+	const int SIZE = 8;
+	string Customer_Number_String;
+	char Customer_Number_Array[SIZE];
+
+	cout << "Please input your customer number in the format of ###@@@@ (8 digits): ";
+	cin >> Customer_Number_String;
+
+	if (Customer_Number_String.length() != SIZE)
+	{
+		cout << "Invalid customer number!" << endl;
+		return;
+	}
+
+	for (int Counting = 0; Counting < SIZE; Counting++)
+	{
+		Customer_Number_Array[Counting] = Customer_Number_String[Counting];
+	}
+
+	if (Program_10_2_B(Customer_Number_Array, SIZE))
+	{
+		cout << "That is a valid customer number." << endl;
+	}
+	else
+	{
+		cout << "Invalid customer number!" << endl;
+	}
+
+}
+
+bool Program_10_2_B(char Customer_Number_Array[], int SIZE)
+{
+	for (int Count = 0; Count < SIZE; Count++)
+	{
+		if (Count <= 2)
+		{
+			if (isalpha(Customer_Number_Array[Count]))
+			{
+				continue;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (isdigit(Customer_Number_Array[Count]))
+			{
+				continue;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+
+void Program_10_3()
+{
+	char User_Input;
+
+	cout << "Please input a character to be turned into an uppercase character: ";
+	cin.get(User_Input);
+
+	cout << User_Input << " as a uppercase character is " << toupper(User_Input) << "." << endl;
+	cout << User_Input << " as a lowercase character is " << tolower(User_Input) << "." << endl;
+}
+
+
+void Program_10_5()
+{
+	const int SIZE = 80;
+	char line[SIZE];
+	int count = 0;
+
+	cout << "Enter a sentence, no longer than 79 characters." << endl;
+	cin.getline(line, SIZE);
+
+	while (line[count] != '\0')
+	{
+		cout << line[count];
+		count++;
+	}
+	cout << endl;
+}
+
+
