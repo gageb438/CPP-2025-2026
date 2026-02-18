0e57bd47b83e2ada6b112013eb386b5dad354711
Gage <gageb534@gmail.com>
2026-02-18 13:56:09 -0600

    Update ConsoleApplication1.cpp

--- Chapter 10/Chapter 10 Exercises/ConsoleApplication1/ConsoleApplication1.cpp
+++ Chapter 10/Chapter 10 Exercises/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,15 +4,19 @@
 
 using namespace std;
 
+void Upperify(const char* Editable_Manipulator);
+
 int main()
 {
 	int Choice;
 
-	cout << "Chapter 10 programming exercises menu:" << endl;
-	cout << "1: Case Manipulator" << endl;
-
 	while (true)
 	{
+		cout << endl;
+
+		cout << "Chapter 10 programming exercises menu:" << endl;
+		cout << "1: Case Manipulator" << endl;
+
 		cout << ":> ";
 		cin >> Choice;
 
@@ -33,23 +37,45 @@ int main()
 		case 1:
 		{
 			string Manipulator;
-			
-
 			cout << "Case Manipulator:" << endl;
 			cout << "Input a string to manipulate: ";
 			
+			cin.ignore();
 			getline(cin, Manipulator);
 
-			char Manipulator_2[] = Manipulator;
+			const char* Un_Editable_Manip = Manipulator.c_str();
+
+			char Editable_Manipulator[2048];
+
+			strcpy_s(Editable_Manipulator, 2048, Un_Editable_Manip);
+
+			cout << "String you input : " << Editable_Manipulator << endl;
+
+			Upperify(Un_Editable_Manip);
+
+			cout << "Uppercased string : " << Editable_Manipulator << endl;
 		}
 		}
 
 	}
 }
 
-void Upperify()
+void Upperify(const char* Un_Editable_Manipulator)
 {
+	char Editable_Manipulator[2048];
+	strcpy_s(Editable_Manipulator, 2048, Un_Editable_Manipulator);
 
+	for (int Iterator = 0; Iterator < 1024; Iterator++)
+	{
+		if (Editable_Manipulator[Iterator] == '\0')
+		{
+			return;
+		}
+		else
+		{
+			Editable_Manipulator[Iterator] = toupper(Editable_Manipulator[Iterator]);
+		}
+	}
 }
 
 void Lowerify()
