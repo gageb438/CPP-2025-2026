b28e826084038b298a1511136fd5a20448dc31a2
Gage <gageb534@gmail.com>
2026-03-03 13:54:54 -0600

    Update Chapter 11 Exercises.cpp

--- Chapter 11/Chapter 11 Exercises/Chapter 11 Exercises.cpp
+++ Chapter 11/Chapter 11 Exercises/Chapter 11 Exercises.cpp
@@ -1,4 +1,6 @@
+// Speakers_Bureau.cpp
 #include <iostream>
+#include <vector> // For speaker tracking
 
 using namespace std;
 
@@ -6,5 +8,19 @@ using namespace std;
 
 int main()
 {
+	/*
+	Main displays a menu with 4 options.
+	1. Enter new speaker information
+	2. Change speaker information
+	3. Display all speaker information
+	4. Exit the program
+	*/
+	const int VALID_MAX = 4, VALID_MIN = 1;
+	int Choice = -1;
 
+	while (Choice < VALID_MIN || Choice > VALID_MAX)
+	{
+		cout << 
+	}
+	
 }
