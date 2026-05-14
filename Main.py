6865acc27b30c77df46fd32df9bd7ed7c2555769
Gage <gageb534@gmail.com>
2026-05-14 13:35:03 -0500

    Deleted old project files

--- Main.py
+++
@@ -1,9 +0,0 @@
-import Reader
-
-My_Reader = Reader.Reader()
-
-Success = My_Reader.Set_Target_File("C:\\Users\\jwboy\\Documents\\GitHub\\CPP-2025-2026\\card_dealer.py")
-
-if Success:
-    My_Reader._Identify_Main_Flags()
-    My_Reader.Output_Flag_List()
